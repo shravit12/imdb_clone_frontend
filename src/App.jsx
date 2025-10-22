adc3322df8dbe547ee9def66d6bf08edddaa64f7
Shravit Sharma <your-email@example.com>
2025-10-22 12:15:16 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import Navbar from './Components/Navbar'
+import Home from './Pages/Home'
+// import './App.css'
+import MovieList from './Pages/MovieList';
+import MovieDetails from './Pages/MovieDetails';
+import {  Routes ,Route} from "react-router-dom";
+import Toprated from './Pages/Toprated';
+import Actordetails from './Pages/Actordetails';
+import ScrollToTop from './Components/ScrollToptop';
+function App() {
+
+  return (
+    <>
+     <Navbar />
+     <ScrollToTop />
+     <Routes>
+      <Route path="/" element={<Home/>}/>
+      <Route path="/movies" element={<MovieList/>}/>
+       <Route path="/movie/:id" element={<MovieDetails/>}/>
+       <Route path="/top-rated" element={<Toprated/>}/>
+       <Route path="/actor/:id" element={<Actordetails/>}/>
+     </Routes>
+    </>
+  )
+}
+
+export default App
